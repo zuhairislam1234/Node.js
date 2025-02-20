35ed9f2d1a2691b14c6b75a7a2a4ed8a7d310adf
zuhairislam1234 <jlu08329@student.jlu.edu.in>
2025-02-20 11:51:40 +0530

    Add files via upload

---
+++ program2.jsx
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter an integer: ', (input) => {
+    const number = parseInt(input, 10);
+
+    if (!isNaN(number)) {
+        console.log(`You entered: ${number}`);
+    } else {
+        console.log('Invalid input. Please enter a valid integer.');
+    }
+
+    rl.close();
+});
