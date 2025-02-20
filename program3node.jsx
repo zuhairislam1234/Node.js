f3056dde97aa3679a84b1270a1961ed32418e69b
zuhairislam1234 <jlu08329@student.jlu.edu.in>
2025-02-20 12:01:37 +0530

    Add files via upload

---
+++ program3node.jsx
@@ -0,0 +1,31 @@
+var rdl = require('readline');
+var rl = rdl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var arr = [1, 2];
+console.log("hello1");
+
+let i = 0;
+
+function askNumber() {
+    if (i < 5) {
+        rl.question("Enter number: ", (y) => {
+            let x = parseInt(y);
+            if (!isNaN(x)) {
+                console.log(`Number: ${x}`);
+                arr.push(x);
+            } else {
+                console.log("Invalid input");
+            }
+            console.log(arr);
+            i++;
+            askNumber(); // Recursive call for next input
+        });
+    } else {
+        rl.close(); // Close readline after all inputs
+    }
+}
+
+askNumber();
