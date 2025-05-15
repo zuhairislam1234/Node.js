12399c548abd8f4172e7ed6a85d2b0bb4c99f789
zuhairislam1234 <jlu08329@student.jlu.edu.in>
2025-05-15 12:34:23 +0530

    Update program3node.jsx

--- program3node.jsx
+++ program3node.jsx
@@ -1,31 +1,43 @@
 var rdl = require('readline');
-var rl = rdl.createInterface({
-    input: process.stdin,
-    output: process.stdout
+
+var rlrdl.createInterface((
+
+input: process.stdin,
+
+output: process.stdout
+
 });
 
 var arr = [1, 2];
-console.log("hello2");
+
+console.log("hello1");
 
 let i = 0;
 
 function askNumber() {
-    if (i < 5) {
-        rl.question("Enter number: ", (y) => {
-            let x = parseInt(y);
-            if (!isNaN(x)) {
-                console.log(`Number: ${x}`);
-                arr.push(x);
-            } else {
-                console.log("Invalid input");
-            }
-            console.log(arr);
-            i++;
-            askNumber();
-        });
-    } else {
-        rl.close();
-    }
-}
+
+if (i <5) {
+
+rl.question("Enter number: ", (y) => {
+
+let x = parseInt(y);
+
+if (!isNaN(x)) {
+
+console.log('Number: ${x}');
+
+arr.push(x);}
+
+else {console.log("Invalid input") }
+
+console.log(arr);
+
+i++;
+
+askNumber(); });
+
+} else {
+
+rl.close(); // Close readline after all inputs}}
 
 askNumber();
