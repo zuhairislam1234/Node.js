f5e793eae7f57b2ddb18a0d118b9d90ecaaa7bf9
zuhairislam1234 <jlu08329@student.jlu.edu.in>
2025-05-15 12:15:39 +0530

    Update node.js program.js

--- node.js program.js
+++ node.js program.js
@@ -1,10 +1,9 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+input: process.stdin,
+output: process.stdout
 });
-
 rl.question('Enter your name: ', (name) => {
-  console.log(`Hello, ${name}!`);
-  rl.close();
+console.log(`Hello, ${name}!`);
+rl.close();
 });
